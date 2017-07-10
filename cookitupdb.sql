99ffdc0552670e8c8f9aade92d6847749976e32c
ntran1321 <nancy.tran.1321@gmail.com>
2017-07-10 17:34:33 -0600

    updated db

--- cookitupdb.sql
+++ cookitupdb.sql
@@ -1,212 +1,272 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 5.6.35, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: cookitupdb
+-- ------------------------------------------------------
+-- Server version	5.6.35
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema cookitupdb
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `cookitupdb` ;
+--
+-- Table structure for table `ingredient`
+--
 
--- -----------------------------------------------------
--- Schema cookitupdb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cookitupdb` DEFAULT CHARACTER SET utf8 ;
-USE `cookitupdb` ;
+DROP TABLE IF EXISTS `ingredient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ingredient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `user` ;
+--
+-- Dumping data for table `ingredient`
+--
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(45) NULL,
-  `password` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+LOCK TABLES `ingredient` WRITE;
+/*!40000 ALTER TABLE `ingredient` DISABLE KEYS */;
+INSERT INTO `ingredient` VALUES (1,'Rice');
+/*!40000 ALTER TABLE `ingredient` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `profile`
+--
 
--- -----------------------------------------------------
--- Table `recipe`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `recipe` ;
+DROP TABLE IF EXISTS `profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profile` (
+  `first_name` int(11) NOT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `profile_pic` varchar(45) DEFAULT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `weight` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`first_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `recipe` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `image_url` VARCHAR(400) NULL,
-  `source_url` VARCHAR(400) NULL,
-  `f2f_url` VARCHAR(400) NULL,
-  `recipe_id` INT NULL,
-  `title` VARCHAR(45) NULL,
-  `publisher` VARCHAR(45) NULL,
-  `publisher_url` VARCHAR(45) NULL,
-  `social_rank` VARCHAR(45) NULL,
-  `ingredients` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `profile`
+--
 
+LOCK TABLES `profile` WRITE;
+/*!40000 ALTER TABLE `profile` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profile` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `profile`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `profile` ;
+--
+-- Table structure for table `rating`
+--
 
-CREATE TABLE IF NOT EXISTS `profile` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `age` INT NULL,
-  `profile_pic` VARCHAR(45) NULL,
-  `location` VARCHAR(45) NULL,
-  `weight` DECIMAL(5,2) NULL,
-  `user_id` INT NOT NULL,
+DROP TABLE IF EXISTS `rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rating` (
+  `id` varchar(45) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `recipe_id` int(11) NOT NULL,
+  `value` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_profile_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_profile_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_user_has_recipe_recipe1_idx` (`recipe_id`),
+  KEY `fk_user_has_recipe_user1_idx` (`user_id`),
+  CONSTRAINT `fk_user_has_recipe_recipe1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_recipe_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `rating`
+--
 
--- -----------------------------------------------------
--- Table `tag`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `tag` ;
+LOCK TABLES `rating` WRITE;
+/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `tag` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `recipe`
+--
 
+DROP TABLE IF EXISTS `recipe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_url` varchar(400) DEFAULT NULL,
+  `source_url` varchar(400) DEFAULT NULL,
+  `f2f_url` varchar(400) DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `publisher` varchar(45) DEFAULT NULL,
+  `publisher_url` varchar(45) DEFAULT NULL,
+  `social_rank` varchar(45) DEFAULT NULL,
+  `recipe_ingredient` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `ingredient`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `ingredient` ;
+--
+-- Dumping data for table `recipe`
+--
 
-CREATE TABLE IF NOT EXISTS `ingredient` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+LOCK TABLES `recipe` WRITE;
+/*!40000 ALTER TABLE `recipe` DISABLE KEYS */;
+INSERT INTO `recipe` VALUES (2,NULL,NULL,NULL,'Fried Rice',NULL,NULL,NULL,1);
+/*!40000 ALTER TABLE `recipe` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `recipe_ingredient`
+--
 
--- -----------------------------------------------------
--- Table `recipe_tag`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `recipe_tag` ;
-
-CREATE TABLE IF NOT EXISTS `recipe_tag` (
-  `id` VARCHAR(45) NOT NULL,
-  `recipe_id` INT NOT NULL,
-  `tag_id` INT NOT NULL,
+DROP TABLE IF EXISTS `recipe_ingredient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipe_ingredient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `recipe_id` int(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
+  `quantity` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_recipe_has_tag_tag1_idx` (`tag_id` ASC),
-  INDEX `fk_recipe_has_tag_recipe_idx` (`recipe_id` ASC),
-  CONSTRAINT `fk_recipe_has_tag_recipe`
-    FOREIGN KEY (`recipe_id`)
-    REFERENCES `recipe` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_recipe_has_tag_tag1`
-    FOREIGN KEY (`tag_id`)
-    REFERENCES `tag` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_recipe_has_ingredient_ingredient1_idx` (`ingredient_id`),
+  KEY `fk_recipe_has_ingredient_recipe1_idx` (`recipe_id`),
+  CONSTRAINT `fk_recipe_has_ingredient_ingredient1` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recipe_has_ingredient_recipe1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `recipe_ingredient`
+--
 
--- -----------------------------------------------------
--- Table `tag_ingredient`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `tag_ingredient` ;
+LOCK TABLES `recipe_ingredient` WRITE;
+/*!40000 ALTER TABLE `recipe_ingredient` DISABLE KEYS */;
+INSERT INTO `recipe_ingredient` VALUES (1,2,1,'2 cups');
+/*!40000 ALTER TABLE `recipe_ingredient` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `tag_ingredient` (
-  `id` VARCHAR(45) NOT NULL,
-  `tag_id` INT NOT NULL,
-  `ingredient_id` INT NOT NULL,
-  INDEX `fk_tag_has_ingredient_ingredient1_idx` (`ingredient_id` ASC),
-  INDEX `fk_tag_has_ingredient_tag1_idx` (`tag_id` ASC),
+--
+-- Table structure for table `recipe_tag`
+--
+
+DROP TABLE IF EXISTS `recipe_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipe_tag` (
+  `id` varchar(45) NOT NULL,
+  `recipe_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_tag_has_ingredient_tag1`
-    FOREIGN KEY (`tag_id`)
-    REFERENCES `tag` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tag_has_ingredient_ingredient1`
-    FOREIGN KEY (`ingredient_id`)
-    REFERENCES `ingredient` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_recipe_has_tag_tag1_idx` (`tag_id`),
+  KEY `fk_recipe_has_tag_recipe_idx` (`recipe_id`),
+  CONSTRAINT `fk_recipe_has_tag_recipe` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recipe_has_tag_tag1` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `recipe_tag`
+--
 
--- -----------------------------------------------------
--- Table `recipe_ingredient`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `recipe_ingredient` ;
+LOCK TABLES `recipe_tag` WRITE;
+/*!40000 ALTER TABLE `recipe_tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recipe_tag` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `recipe_ingredient` (
-  `id` VARCHAR(45) NOT NULL,
-  `recipe_id` INT NOT NULL,
-  `ingredient_id` INT NOT NULL,
-  `quanitiy` VARCHAR(45) NULL,
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tag`
+--
+
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tag_ingredient`
+--
+
+DROP TABLE IF EXISTS `tag_ingredient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag_ingredient` (
+  `id` varchar(45) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_recipe_has_ingredient_ingredient1_idx` (`ingredient_id` ASC),
-  INDEX `fk_recipe_has_ingredient_recipe1_idx` (`recipe_id` ASC),
-  CONSTRAINT `fk_recipe_has_ingredient_recipe1`
-    FOREIGN KEY (`recipe_id`)
-    REFERENCES `recipe` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_recipe_has_ingredient_ingredient1`
-    FOREIGN KEY (`ingredient_id`)
-    REFERENCES `ingredient` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_tag_has_ingredient_ingredient1_idx` (`ingredient_id`),
+  KEY `fk_tag_has_ingredient_tag1_idx` (`tag_id`),
+  CONSTRAINT `fk_tag_has_ingredient_ingredient1` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tag_has_ingredient_tag1` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `tag_ingredient`
+--
 
--- -----------------------------------------------------
--- Table `rating`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `rating` ;
+LOCK TABLES `tag_ingredient` WRITE;
+/*!40000 ALTER TABLE `tag_ingredient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag_ingredient` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `rating` (
-  `id` VARCHAR(45) NOT NULL,
-  `user_id` INT NOT NULL,
-  `recipe_id` INT NOT NULL,
-  `value` INT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_user_has_recipe_recipe1_idx` (`recipe_id` ASC),
-  INDEX `fk_user_has_recipe_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_user_has_recipe_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_has_recipe_recipe1`
-    FOREIGN KEY (`recipe_id`)
-    REFERENCES `recipe` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Table structure for table `user`
+--
 
-SET SQL_MODE = '';
-GRANT USAGE ON *.* TO cooker;
- DROP USER cooker;
-SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-CREATE USER 'cooker' IDENTIFIED BY 'cooker';
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'cooker';
-GRANT SELECT, INSERT, TRIGGER ON TABLE * TO 'cooker';
-GRANT ALL ON * TO 'cooker';
+--
+-- Dumping data for table `user`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'test@test.com','pw123');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-10 17:31:54
