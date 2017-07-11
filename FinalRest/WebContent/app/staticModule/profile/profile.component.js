9a54b6492fe322a1da837c7c2e7c070737f06d95
Ryan Grillo <ryangrillo@gmail.com>
2017-07-11 15:59:51 -0600

    created profile form

--- FinalRest/WebContent/app/staticModule/profile/profile.component.js
+++ FinalRest/WebContent/app/staticModule/profile/profile.component.js
@@ -3,11 +3,21 @@ angular.module('staticModule')
 	templateUrl : "app/staticModule/profile/profile.component.html",
 		controller: function(staticService){
 			var vm = this;
+			vm.showCreateButton = true;
 			
 			vm.createProfile = function(profile){
 				staticService.create(profile)
 				.then(function(res){
-					$location.path('/');
+					$location.path('/profile');
+				})
+			}
+			vm.showCreate = function(){
+				vm.showCreateButton = false;
+			}
+			vm.updateProfile = function(profile){
+				staticService.update(profile)
+				.then(function(res){
+					$location.path('/profile');
 				})
 			}
 		},
