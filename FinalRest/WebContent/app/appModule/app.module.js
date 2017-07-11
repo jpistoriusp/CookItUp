d5c269f06cbeacb550b15231a519d03d5ce66630
Eric Lee <ericlee@Daewoos-Mac.local>
2017-07-11 11:06:22 -0600

    added path to login page and create

--- FinalRest/WebContent/app/appModule/app.module.js
+++ FinalRest/WebContent/app/appModule/app.module.js
@@ -3,6 +3,14 @@ angular.module('appModule',['staticModule', 'ngRoute', 'recipe','authModule'])
 		$routeProvider
 			.when('/profile', {
 				template : '<profile></profile>'
+			}).when('/login', {
+			
+				template : "<login></login>"
+				
+			}).when('/create', {
+			
+				template : "<create></create>"
+				
 			})
 			.otherwise({
 				template : '<not-found></not-found>'
