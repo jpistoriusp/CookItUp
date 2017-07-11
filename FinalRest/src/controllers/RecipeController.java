84af1901fcef63944dbf00579614b1ec84b5c8e8
Miles Grandin <miles.grandin@gmail.com>
2017-07-11 11:43:44 -0600

    working on RecipeController & search based on ingredients

---
+++ FinalRest/src/controllers/RecipeController.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import data.RecipeDAO;
+import entities.Recipe;
+
+@RestController
+public class RecipeController {
+	
+	@Autowired
+	private RecipeDAO recipedao;
+	
+	@RequestMapping(path = "user/{uid}/recipe", method = RequestMethod.GET)
+	public Collection<Recipe> index(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid) {
+		return recipedao.index(uid);
+	}
+  
+	@RequestMapping(path = "user/{uid}/recipe/{rid}", method = RequestMethod.GET)
+	public Recipe show(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid, @PathVariable int rid){
+		return recipedao.show(uid, rid);
+	}
+  
+	@RequestMapping(path = "user/{uid}/recipe", method = RequestMethod.POST)
+	public Recipe create(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid, @RequestBody String recipeJson){
+		return recipedao.create(uid, recipeJson);
+	}
+  
+	@RequestMapping(path = "user/{uid}/recipe/{rid}", method = RequestMethod.PUT)
+	public Recipe update(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid, @PathVariable int rid, @RequestBody String recipeJson){
+		return recipedao.update(uid, rid, recipeJson);
+	}
+  
+	@RequestMapping(path = "user/{uid}/recipe/{rid}", method = RequestMethod.DELETE)
+	public Boolean destroy(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid, @PathVariable int rid) {
+		return recipedao.destroy(uid, rid);
+	}
+}
