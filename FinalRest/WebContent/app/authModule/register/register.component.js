2592e0c750d77686ef3af266ffb59f5f5065d4c7
Eric Lee <ericlee@Daewoos-Mac.local>
2017-07-11 10:47:14 -0600

    added confirm password

--- FinalRest/WebContent/app/authModule/register/register.component.js
+++ FinalRest/WebContent/app/authModule/register/register.component.js
@@ -1,13 +1,17 @@
-angular.module('authModule').component('register',{
-	templateUrl :'app/authModule/register/register.component.html',
-	controller : function(authService, $location){
+angular.module('authModule').component('register', {
+	templateUrl : 'app/authModule/register/register.component.html',
+	controller : function(authService, $location) {
 		var vm = this;
-		
-		vm.register = function (user){
-			authService.register(user)
-			.then(function(res){
-				$location.path('/todo')
-			});
+		vm.errors = [];
+		vm.register = function(user) {
+			if (user.password == user.password2) {
+				authService.register(user).then(function(res) {
+					//must add path
+					$location.path('')
+				});
+			} else {
+				errors.push('The password does not match. Please make sure you typed in the same password.');
+			}
 		}
 	},
 	controllerAs : 'vm'
