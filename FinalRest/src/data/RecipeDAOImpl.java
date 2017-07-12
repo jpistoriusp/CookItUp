4413f78df376a226cfbb033d244d0ac212773576
Miles Grandin <miles.grandin@gmail.com>
2017-07-12 13:22:30 -0600

    minor changes

--- FinalRest/src/data/RecipeDAOImpl.java
+++ FinalRest/src/data/RecipeDAOImpl.java
@@ -44,7 +44,6 @@ public class RecipeDAOImpl implements RecipeDAO{
 				managed = em.createQuery(ingredientQuery, Ingredient.class)
 						.setParameter("name", ingd.getName()).getSingleResult();
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 				continue;
 			}
