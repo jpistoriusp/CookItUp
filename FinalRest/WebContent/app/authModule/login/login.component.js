9e73dede081d506abc5357d7e8b8a1f54c8530e8
Eric Lee <ericlee@Daewoos-Mac.local>
2017-07-12 11:15:41 -0600

    added message to confirm email and password

--- FinalRest/WebContent/app/authModule/login/login.component.js
+++ FinalRest/WebContent/app/authModule/login/login.component.js
@@ -2,14 +2,17 @@ angular.module('authModule').component('login',{
 	templateUrl :'app/authModule/login/login.component.html',
 	controller : function(authService,$location){
 		var vm = this;
-		
+		vm.notvalid= true;
 		vm.login = function (user){
 			authService.login(user)
 			.then(function(res){
 				// must create path
+				vm.notvalid= true;
 				$location.path('/');
 			})
 			.catch(function(e){
+				vm.notvalid= false;
+				console.log('ahsdfhashdfhas');
 				$location.path('/login');
 			});
 			
