33bd93cc9c007db0f9b357f12df3d015d9632057
Eric Lee <ericlee@Daewoos-Mac.local>
2017-07-13 17:04:00 -0600

    added delete review for admin.

--- FinalRest/WebContent/app/moderator/mod/mod.component.js
+++ FinalRest/WebContent/app/moderator/mod/mod.component.js
@@ -9,7 +9,15 @@ angular.module('mod').component('mod', {
 				vm.reviewList = result.data;
 			});
 		}
-		vm.favorite();
+		
+	vm.deleteReview = function(id){
+		modService.del(id)
+		.then(function(result){
+			vm.reviewList.splice(id-1);
+		});
+		}
+		
+		vm.listReview();
 	},
 	controllerAs : 'vm'
 });
--- FinalRest/src/data/ModDAOImpl.java
+++ FinalRest/src/data/ModDAOImpl.java
@@ -23,5 +23,21 @@ public class ModDAOImpl implements ModDAO {
 		return r;
 	
 	}
+	
+	public boolean delReview(int id){
+		Rating r;
+		try {
+			String query = "SELECT r FROM Rating r where r.id = :id";
+			r = em.createQuery(query, Rating.class).setParameter("id", id).getSingleResult();
+			em.remove(r);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+		
+	}
 
 }
