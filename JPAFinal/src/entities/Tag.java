65d913e34e13133bac31c32e762b87a396e4598a
Paul Pistorius <jpistoriusp@gmail.com>
2017-07-13 14:59:15 -0600

    removed getters and setters method

--- JPAFinal/src/entities/Tag.java
+++ JPAFinal/src/entities/Tag.java
@@ -52,15 +52,6 @@ public class Tag {
 	public void setIngredient(List<Ingredient> ingredient) {
 		this.ingredient = ingredient;
 	}
-	
-
-	public boolean isSelected() {
-		return selected;
-	}
-
-	public void setSelected(boolean selected) {
-		this.selected = selected;
-	}
 
 	public int getId() {
 		return id;
