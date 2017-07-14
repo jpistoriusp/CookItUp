e7f9776fc7ff8ad638c5357f82fc695c4af5a068
Eric Lee <ericlee@Daewoos-Mac.local>
2017-07-14 16:07:23 -0600

    added bootstrap

--- FinalRest/WebContent/app/staticModule/home/home.component.js
+++ FinalRest/WebContent/app/staticModule/home/home.component.js
@@ -1,9 +1,18 @@
 angular.module('staticModule')
 	.component('home', {
 		templateUrl : "app/staticModule/home/home.component.html",
-		controller: function(staticService, $location){
+		controller: function(staticService, $location, authService){
 			
 			var vm = this;
+			
+			vm.loginChecker = function() {
+				if (authService.getToken().id) {
+					console.log('true');
+					return true;
+				}
+				console.log('false');
+				return false;
+			}
 			
 			vm.displayProfile = function(){
 				console.log("in display profile")
