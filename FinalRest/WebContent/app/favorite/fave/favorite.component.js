31534f3e25f717d831aca4a79daea5d195b7ce8d
Miles Grandin <miles.grandin@gmail.com>
2017-07-17 17:39:58 -0600

    cleaning up the favorites view - can click faves to view the recipe

--- FinalRest/WebContent/app/favorite/fave/favorite.component.js
+++ FinalRest/WebContent/app/favorite/fave/favorite.component.js
@@ -1,17 +1,52 @@
 angular.module('fave').component('fave', {
 	templateUrl : 'app/favorite/fave/favorite.component.html',
-	controller : function(faveService,authService) {
+	controller : function(faveService,authService,recipeService,$rootScope) {
 		var vm = this;
 		vm.errors = [];
 		vm.listFave = [];
+		vm.selected = {};
+		vm.showList = true;
 		
 		vm.favorite = function(){
 			faveService.getFaveList(authService.getToken().id)
 			.then(function(result){
 				vm.listFave = result.data;
-				console.log(vm.listFave);
+				vm.listFave.forEach(function(f,idx,arr){
+					var total = 0;
+					f.recipe.rating.forEach(function(r,idx,arr){
+						total += r.value;
+					})
+					f.recipe.avgRating = total/f.recipe.rating.length;
+				})
 			});
 		}
+		
+		vm.toggle = function(fave){
+			vm.selected = fave.recipe;
+			vm.showList = false;
+			vm.loadDetails(vm.selected);
+			
+			$rootScope.$broadcast('recipeSelected', {
+				value : vm.showList
+			})
+		}
+		
+		vm.loadDetails = function (recipe) {
+			console.log(recipe)
+			recipeService.showIngredients(recipe)
+				.then(function(response){
+					vm.selected.recipeIngredients = response.data;
+					recipeService.showInstructions(recipe)
+						.then(function(response){
+							vm.selected.instructions = response.data;
+						})
+				})
+		}
+		
+		vm.changeListVisibility = function() {
+			vm.showList = vm.showList ? false : true;
+		}
+		
 		vm.favorite();
 	},
 	controllerAs : 'vm'
